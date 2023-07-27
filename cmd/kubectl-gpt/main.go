84450a9171eb0cbc926a45b8512a891873231d89
Ghani, Maaz <Maaz.Ghani@netapp.com>
2023-07-27 04:57:52 -0400

    customize

--- cmd/kubectl-gpt/main.go
+++ cmd/kubectl-gpt/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"bufio"
+    "bufio"
 	"flag"
 	"fmt"
 	"os"
 	"os/exec"
-	"strconv"
+    "strconv"
 	"strings"
-	"sync"
-	"time"
 
-	gpt "github.com/devinjeon/kubectl-gpt/pkg/gpt"
+	gpt "github.com/maazghani/kubectl-gpt/pkg/gpt"
 )
 
 const (
@@ -22,11 +20,10 @@ const (
 )
 
 var (
-	version     string
-	yesFlag     = flag.Bool("yes", false, "Execute the generated command without asking for confirmation")
-	noFlag      = flag.Bool("no", false, "Print the generated command without executing it")
-	helpFlag    = flag.Bool("help", false, "Show usage information")
-	versionFlag = flag.Bool("version", false, "Show the version")
+	version        string
+	autoApproveFlag = flag.Bool("auto-approve", false, "Execute the generated command without asking for confirmation")
+	helpFlag        = flag.Bool("help", false, "Show usage information")
+	versionFlag     = flag.Bool("version", false, "Show the version")
 )
 
 func main() {
@@ -63,34 +60,23 @@ func main() {
 	}
 	request := gpt.NewOpenAIRequest(model, temperature, maxTokens, systemMessage, query)
 
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go printLoadingMessage(&wg)
-
 	response, err := gpt.RequestChatGptAPI(request, apiKey)
-
-	wg.Done()
-	fmt.Printf("\033[2K\r") // Clear loading message after completion
 	if err != nil {
 		fmt.Printf("Failed to call OpenAI API: %v\n", err)
 		os.Exit(1)
 	}
 
 	kubectlCommand := extractCommand(response)
-	fmt.Println("\033[1;31m❗[WARNING] Please verify the generated commands before executing them on your k8s cluster,",
-		"especially `update` and `patch` commands, as GPT-generated commands may be inaccurate.\033[0m")
 	fmt.Printf("\033[1;34m[Generated Command]\033[0m\n%s\n", kubectlCommand)
 
-	if *noFlag {
-		os.Exit(0)
-	}
-	if !*yesFlag {
+	if !*autoApproveFlag {
 		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("\033[1;34m⎈ Do you really want to execute this command?\033[0m [y/N]: ")
+		fmt.Print("\033[1;34m⎈ Press enter to execute the command or type 'abort' to cancel.\033[0m: ")
 		text, _ := reader.ReadString('\n')
 		text = strings.Replace(text, "\n", "", -1)
 
-		if strings.ToLower(text) != "y" {
+		if strings.ToLower(text) == "abort" {
+			fmt.Println("Command execution aborted.")
 			os.Exit(0)
 		}
 	}
@@ -139,48 +125,21 @@ func getOpenAIConfigFromEnv() (apiKey string, model string, temperature float64,
 	return apiKey, model, temperature, maxTokens
 }
 
-func printLoadingMessage(wg *sync.WaitGroup) {
-	loading := "Getting kubectl command from GPT API "
-	i := 0
-
-	loadingSymbol := "🚶"
-
-	loadingTicker := time.NewTicker(time.Millisecond * 500)
-
-	go func() {
-		for {
-			select {
-			case <-loadingTicker.C:
-				if i%2 == 0 {
-					loadingSymbol = "🚶"
-				} else {
-					loadingSymbol = "🏃"
-				}
-				fmt.Printf("\033[2K\r%s %s%s", loadingSymbol, loading, strings.Repeat(".", i%30))
-				i++
-			}
-		}
-	}()
-
-	wg.Wait()
-	loadingTicker.Stop()
-}
-
 func printHelp() {
 	fmt.Println("Usage: kubectl-gpt [OPTIONS] QUERY")
 	fmt.Println("Translate the given query to a kubectl command using OpenAI GPT API.")
 	fmt.Println()
 	fmt.Println("Options:")
-	fmt.Println("  --yes           Execute the generated command without asking for confirmation")
-	fmt.Println("  --no            Print the generated command without executing it")
-	fmt.Println("  --help          Show this message and exit")
-	fmt.Println("  --version       Show the version")
+	fmt.Println("  --auto-approve   Execute the generated command without asking for confirmation")
+	fmt.Println("  --help           Show this message and exit")
+	fmt.Println("  --version        Show the version")
 	fmt.Println()
 	fmt.Println("Environment variables:")
 	fmt.Println("  OPENAI_API_KEY        OpenAI API Key")
 	fmt.Println("  OPENAI_MODEL          OpenAI Model to use (default is gpt-3.5-turbo)")
 	fmt.Println("  OPENAI_TEMPERATURE    Temperature for the OpenAI request (default is 0.2)")
 	fmt.Println("  OPENAI_MAX_TOKENS     Max tokens for the OpenAI request (default is 300)")
+	fmt.Println("  KUBEGPT_MODE          Set to 'auto' to automatically approve generated commands without prompting")
 }
 
 func extractCommand(response gpt.OpenAIResponse) string {
